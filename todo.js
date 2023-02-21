4f9ce520250de2c50d6f38261cda31643d93e9af
andreacasanova1 <116755988+andreacasanova1@users.noreply.github.com>
2023-02-21 15:43:02 +0100

    input clonados

--- todo.js
+++ todo.js
@@ -1,2 +1,22 @@
- console.log('hola')
+const listaDeTodos = [];
+
+function addTodo() {
+  // Seleccionamos el elemnto con la clase que quiero encontrar (el input);
+  const referenciaAlInput = document.querySelector('.todo-input')
+  // Accedemos a la propiedad .value (valor) del input, y la añadimos a la lista
+  listaDeTodos.push(referenciaAlInput.value);
+
+  // Sacamos esta linea del bucle y la ponemos aqui para no limpiar en cada iteración
+  const elementoContenedor = document.querySelector('.todo-list');
+  // Le decimos al contenedor, que su html es = a '', es decir, a nada
+  elementoContenedor.innerHTML = '';
+
+  listaDeTodos.forEach(function(elemento) {
+    elementoContenedor.innerHTML = elementoContenedor.innerHTML + `<div class="todo">
+      <input type="checkbox">
+      <label>${elemento}</label>
+    </div>`
+  });
+}
   
+  //el nombre la la funcion debe coicidir con el del evento//
